8592a3ece29babef8efd5cdd86eb884c814fd215
Eric Arwido Damanik <arwido.eric@gmail.com>
2024-05-09 17:36:46 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,110 @@
+
+# Sentinel Siege
+Sentinel Siege adalah permainan Tower Defense Game yang memiliki tema Medieval dan menggunakan Pixel Art agar lebih mudah di jalankan di laptop kentang
+
+
+
+## Kelompok OkSobatKoding
+
+- [@Jhoel Robert](https://github.com/Arkyna)
+
+- [@Alwi Arfan Solin](https://github.com/samanbrembo14)
+
+- [@Eric Arwido Damanik](https://github.com/erc-a)
+
+- [@Lucas Hamonangan Simare-Mare](https://github.com/LucasHamonangan412)
+
+- [@Dyo Dwi Carol](https://github.com/Dyocarol)
+
+- [@Afif Malik Azhar](https://github.com/AfifMalikAzhar)
+
+
+## How To Play
+- Pada awal permainan, pemain akan diberikan jumlah gold tertentu untuk membeli tower 
+
+- Bunuh Monster agar mendapatkan gold dalam jumlah tertentu
+
+- Ketika gold sudah cukup, beli tower atau upgrade tower yang sudah ada agar musuh lebih mudah di Bunuh
+
+- Base memiliki HP (Healt Point)
+
+- Ketika monster sudah mencapai base, maka HP base akan berkurang
+
+- ketika sudah mencapai waktu tertentu, boss monster akan datang untuk menyerang base
+
+- ketika boss dikalahkan maka permainan berakhir dan game selesai
+
+- Jika HP base sudah mencapai 0 maka permainan berakhir dan pemain kalah
+
+
+
+## Depedency
+
+```bash
+  Python v3.8.X
+  Pygames v2.5.2
+  math lib
+```
+
+
+## Installation
+
+Untuk Menjalankan project, install python ke dalam laptop atau computer anda dan ketika sudah terinstall, buka terminal yang anda pakai dan gunakan perintah
+
+### For Windows
+```bash
+  pip install pygame
+```
+### For Mac
+```bash
+  pip3 install pygame
+```
+
+### For Linux
+```bash
+  sudo apt-get install python3-pygame
+```
+
+Dan silahkan menjalankan file initialize.py
+    
+## UML Class Sentinel Siege
+![App Screenshot](./assets/images/UML_Game/uml_game1.png)
+
+## Screenshot Game 
+![App Screenshot](https://via.placeholder.com/468x300?text=App+Screenshot+Here)
+
+### Screenshot Map
+![App Screenshot](./assets/images/map/level1.png)
+
+### Screenshot Tower
+![App Screenshot](https://via.placeholder.com/468x300?text=App+Screenshot+Here)
+
+![App Screenshot](https://via.placeholder.com/468x300?text=App+Screenshot+Here)
+
+![App Screenshot](https://via.placeholder.com/468x300?text=App+Screenshot+Here)
+
+### Screenshot Monster
+![App Screenshot](https://via.placeholder.com/468x300?text=App+Screenshot+Here)
+
+![App Screenshot](https://via.placeholder.com/468x300?text=App+Screenshot+Here)
+
+
+
+
+
+
+## Contributors
+
+
+
+| Nama |  NIM     | Kontribusi                |
+| :-------- | :------- | :------------------------- |
+| Alwi Arfan Solin | 122140197 | Asset Search, Programmer |
+| Jhoel Robert Hutagalung | 122140174 | Programmer  |
+| Eric Arwido Damanik | 122140157 | Project Leader,  Programmer   |
+| Lucas Hamonangan Simaremare | 122140196 | Programmer |
+| Dyo Dwi Carol Bukit | 122140145  | Programmer |
+| Afif Malik Azhar | 120140117 | Programmer |
+
+## Reference
+
---
+++ initialize.py
@@ -0,0 +1,170 @@
+import pygame as pgm
+import json
+from bin import globalvar as val
+from bin.monster import Monster
+from bin.world import World
+from bin.button import Button
+from bin.tower import Tower
+
+# initialisasi
+pgm.init()
+
+# create clock
+clock = pgm.time.Clock()
+
+# creating game window
+screen = pgm.display.set_mode((val.SCREEN_WIDTH + val.SIDE_PANEL, val.SCREEN_HEIGHT))
+pgm.display.set_caption(val.GAME_NAME)
+
+
+# Game Variables
+placing_tower = False
+
+
+# Load images
+# map
+map_image = pgm.image.load(r'assets\images\map\level1.png').convert_alpha()
+
+# tower sprite sheet
+tower_sheet = pgm.image.load(r'assets\images\towers\weapon_heavy_arrow.png').convert_alpha()
+
+# tower sprite below tower sheet
+base_tower = pgm.image.load(r'assets\images\towers\tower1.png').convert_alpha()
+
+# individual tower image for mouse cursor
+cursor_tower = pgm.image.load(r'assets\images\towers\tower1.png').convert_alpha()
+
+#enemies
+monster_image = pgm.image.load(r'assets\images\monsters\enemy1.png').convert_alpha()
+
+# buttons
+buy_tower_image = pgm.image.load(r'assets\images\buttons\buy_button.png').convert_alpha()
+cancel_button_image = pgm.image.load(r'assets\images\buttons\cancel_button.png').convert_alpha()
+
+# sidebar 
+sidebar_image = pgm.image.load(r'assets\images\gui\sidepanel.png').convert_alpha()
+
+# load json data for monster path in levels
+with open('bin\levels\level1.tmj') as file:
+    world_data = json.load(file)
+
+# audio
+pgm.mixer.music.load(r"assets\audios\bgm.ogg")
+pgm.mixer.music.play(-1)
+
+
+# creating tower
+def create_tower(mouse_pos):
+    mouse_tile_x = mouse_pos[0] // val.TILE_SIZE
+    mouse_tile_y = mouse_pos[1] // val.TILE_SIZE
+
+    # calculating squential numbers of tile on level
+    mouse_tile_num = (mouse_tile_y * val.COLS) + mouse_tile_x
+
+    # checking tile if placeable
+    if world.tile_map[mouse_tile_num] == 74:
+        # checking the place is already occupied by tower
+        space_is_free = True
+        for tower in tower_groups:
+            if (mouse_tile_x, mouse_tile_y) == (tower.tile_x, tower.tile_y):
+                space_is_free = False
+        # if free then place tower
+        if space_is_free == True:
+            new_tower = Tower(base_tower, tower_sheet, mouse_tile_x, mouse_tile_y)
+            tower_groups.add(new_tower)
+
+
+# create world
+world = World(world_data, map_image)
+world.process_data()
+
+# creating groups
+monster_groups = pgm.sprite.Group()
+tower_groups = pgm.sprite.Group()
+
+
+monster = Monster(world.waypoints, monster_image)
+monster_groups.add(monster)
+
+# create button
+tower_button = Button(val.SCREEN_WIDTH + 30, 120, buy_tower_image, True)
+cancel_button = Button(val.SCREEN_WIDTH + 30, 180, cancel_button_image, True)
+
+# game loop
+run = True
+while run:
+
+    # limits FPS to 60
+    clock.tick(val.FPS) 
+
+    #########################
+    # UPDATING SECTION
+    #########################
+
+    #update groups
+    monster_groups.update()
+    tower_groups.update()
+
+    #########################
+    # DRAWING SECTION
+    #########################
+
+    #screen fill
+    screen.fill("grey100")
+
+    #draw level
+    world.draw(screen)
+
+    #monster path
+    pgm.draw.lines(screen, "grey0", False, world.waypoints)
+
+    #draw groups
+    for tower in tower_groups:
+        # Draw tower base
+        screen.blit(tower.base_tower, tower.base_rect)
+        # Draw tower animation frame
+        screen.blit(tower.image, tower.rect)
+
+    monster_groups.draw(screen)
+    tower_groups.draw(screen)
+
+    #draw sidebar
+    screen.blit(sidebar_image,(960, 0))
+
+    #draw buttons
+    #button for placing tower
+    if tower_button.draw(screen):
+        placing_tower = True
+    # if placing then show the cancel button
+    if placing_tower == True:
+        # show cursor tower
+        cursor_rect = cursor_tower.get_rect()
+        cursor_pos = pgm.mouse.get_pos()
+        cursor_rect.center = cursor_pos
+        if cursor_pos[0] <= val.SCREEN_WIDTH:
+            screen.blit(cursor_tower, cursor_rect)
+
+        if cancel_button.draw(screen):
+            placing_tower = False
+            #print(tower_groups)
+
+    #event handler
+    for event in pgm.event.get():
+            #quit program
+        if event.type == pgm.QUIT:
+            run = False
+
+    #mouse click
+    if event.type == pgm.MOUSEBUTTONDOWN and event.button == 1:
+        mouse_pos = pgm.mouse.get_pos()
+
+        #check if the mouse on the game
+        if mouse_pos[0] < val.SCREEN_WIDTH and mouse_pos[1] < val.SCREEN_HEIGHT:
+            if placing_tower == True:
+                create_tower(mouse_pos)
+         
+        
+    #update display
+    pgm.display.flip()
+
+pgm.quit()
